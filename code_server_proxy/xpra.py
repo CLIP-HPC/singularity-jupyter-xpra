9d3e5b33947b7b004095114f79931ca178772421
Erich Birngruber <erich.birngruber@gmi.oeaw.ac.at>
2020-12-04 21:01:03 +0100

    add basic xterm script

--- code_server_proxy/xpra.py
+++ code_server_proxy/xpra.py
@@ -1,8 +1,10 @@
 # see https://jupyter-server-proxy.readthedocs.io/en/latest/server-process.html#specifying-config-from-python-packages
 def setup_xpra():
   return {
-    # batchspawner-singleuser code-server --bind-addr 0.0.0.0 --auth none
-    'command': ['xpra', 'start', '--bell=no', '--daemon=no', '--clipboard=yes', '--system-tray=yes', '--exit-with-children=yes', '--no-pulseaudio', '--bind-tcp=0.0.0.0:{port}', '--html=on', '--start-child=${XPRA_RUN_SCRIPT}']
+    #
+    'command': ['xpra', 'start', '--bell=no', '--daemon=no', '--clipboard=yes', '--system-tray=yes', '--exit-with-children=yes', '--no-pulseaudio', '--bind-tcp=0.0.0.0:{port}', '--html=on', '--start-child=/usr/local/bin/xterm.sh'],
+    # we caan take up to xx seconds to get ready
+    'timeout': 60
   }
 
 # singularity exec \
---
+++ xterm.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash -l
+
+/usr/bin/xterm &
+
+wait $!
